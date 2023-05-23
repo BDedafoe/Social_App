f5f93fe1edd31bfd532e3685f2fef20581ede8f9
BDedafoe <bmd6260@gmail.com>
2023-05-23 14:26:07 -0500

    .env updated

--- server/index.js
+++ server/index.js
@@ -67,3 +67,7 @@ mongoose
     // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
+
+  app.get('/', (req, res) => {
+    res.send('Welcome to the server')
+  });
